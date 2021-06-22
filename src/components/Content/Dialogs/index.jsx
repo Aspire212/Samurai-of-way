0f2bbde2e326dbfbeda6b97d45b4c50ce3d45e5d
Stanislau <1ven970@gmail.com>
2021-06-22 18:28:16 +0300

    21 dialogs

--- src/components/Content/Dialogs/index.jsx
+++ src/components/Content/Dialogs/index.jsx
@@ -5,8 +5,27 @@ import './Dialogs.css'
 
 
 const Dialogs = () => (
-  <div>
-    Dialogs
+  <div className='dialogs'>
+    <div className='dialogs-items'>
+      <div className="dialog dialog-active">
+        Stason
+      </div>
+      <div className="dialog">
+        Dimych
+      </div>
+      <div className="dialog">
+        Artyom
+      </div>
+      <div className="dialog">
+        Alesya
+      </div>
+
+    </div>
+    <div className='messages'>
+      <div className="message">Hi</div>
+      <div className="message">How is your it-kamsutra?</div>
+      <div className="message">Yo</div>
+    </div>
   </div>
 )
 
